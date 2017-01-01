15336449394aba9c071ed1d2e8e24fe6d8c97314
Neivin Mathew <neivin93@gmail.com>
2017-01-02 00:36:07 +0400

    write down some ideas

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-#Wallgen
+# Wallgen
+A simple abstract wallpaper generator written in Python
+
+## Potential Features
+- Rectangles
+- Triangles
+- Vert/Horizontal bands
+- Just pixels
+- Rotate rectangles to make overlapping diamonds VVVVVV
--- wallgen.py
+++ wallgen.py
@@ -1,15 +1,31 @@
+#!/usr/bin/python3
+
 import sys
 import random
 from PIL import Image, ImageDraw
 
-im = Image.new('RGB', (500,500), color=0)
+default_resolution = {1920, 1080}
+
+im = Image.new('RGB', (500,500))
 
 draw = ImageDraw.Draw(im)
-draw.line((0,0) + im.size, fill = 128)
-draw.line((0, im.size[1], im.size[0], 0), fill=128)
+#draw.line((0,0) + im.size, fill = 128)
+#draw.line((0, im.size[1], im.size[0], 0), fill=128)
+
+for i in range(0,)
+
+
 del draw
 
-im.save('out', "PNG")
-class ColorfulWall:
-:wq
+im.save('out.png', "PNG")
+
+
+
+"""
+class WallGen:
+	def __init__(self):
+		pass
+"""
+
+
 
